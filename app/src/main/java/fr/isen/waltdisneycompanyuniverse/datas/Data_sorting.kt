efa542b252dfde458745450e9b0bc87376cf87fe
Malo-35 <malo.barbaud@isen.yncrea.fr>
2026-03-10 15:41:32 +0100

    Added classes for handling the categories and it's content.

--- app/src/main/java/fr/isen/waltdisneycompanyuniverse/datas/Data_sorting.kt
+++ app/src/main/java/fr/isen/waltdisneycompanyuniverse/datas/Data_sorting.kt
@@ -1,24 +1,24 @@
 package fr.isen.waltdisneycompanyuniverse.datas
 data class Category(
-    val categorie: String = "",
-    val franchises: List<Franchise> = emptyList()
+    var categorie: String = "",
+    var franchises: List<Franchise> = emptyList()
 )
 
 data class Franchise(
-    val id: String = "",
-    val nom: String = "",
-    val sous_sagas: List<SousSaga> = emptyList()
+    var id: String = "",
+    var nom: String = "",
+    var sous_sagas: List<SousSaga> = emptyList()
 )
 
 data class SousSaga(
-    val nom: String = "",
-    val films: List<Film> = emptyList()
+    var nom: String = "",
+    var films: List<Film> = emptyList()
 )
 
 data class Film(
-    val id: String = "",
-    val numero: Int = 0,
-    val titre: String = "",
-    val annee: Int = 0,
-    val genre: String = ""
+    var id: String = "",
+    var numero: Int = 0,
+    var titre: String = "",
+    var annee: Int = 0,
+    var genre: String = ""
 )

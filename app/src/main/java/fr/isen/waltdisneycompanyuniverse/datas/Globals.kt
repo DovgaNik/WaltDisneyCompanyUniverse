46291458cc419ff48442cb2f7565c00b2dfbcc91
Malo-35 <malo.barbaud@isen.yncrea.fr>
2026-03-13 11:04:05 +0100

    Adding a settable amount of maximums per display.

--- app/src/main/java/fr/isen/waltdisneycompanyuniverse/datas/Globals.kt
+++ app/src/main/java/fr/isen/waltdisneycompanyuniverse/datas/Globals.kt
@@ -1,3 +1,6 @@
 package fr.isen.waltdisneycompanyuniverse.datas
 
-val sagas_and_films : String = "categories"
+val sagas_and_films: String = "categories"
+val max_franchises_per_page: Int = 10
+val max_sagas_per_page: Int = 5
+val max_films_per_page: Int = 7
